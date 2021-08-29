90349140e4e75338aacb2bcf19315ecb3be7c0be
OmiSingh2004 <omisinghdbz@gmail.com>
2021-08-29 20:28:22 +0530

    pet

--- src/main/java/main/Bot.java
+++ src/main/java/main/Bot.java
@@ -1,9 +1,7 @@
 package main;
 
-import com.husky.Commands.test.TestCommandsEvent;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
@@ -28,12 +26,15 @@ public class Bot extends ListenerAdapter {
         commands.queue();
     }
 
+    /*
     @Override
     public void onSlashCommand(SlashCommandEvent event){
         switch(event.getName()){
             case "say": TestCommandsEvent.say(event); break;
         }
     }
+
+     */
 }
 
 
