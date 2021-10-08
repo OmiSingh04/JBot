259341611a105a0f3540e183c5245d5d1f5d2bfe
OmiSingh2004 <omisinghdbz@gmail.com>
2021-10-08 15:37:03 +0530

    weeeee

--- src/main/java/com/husky/Bot.java
+++ src/main/java/com/husky/Bot.java
@@ -3,10 +3,7 @@ package com.husky;
 import com.husky.events.commands.Command;
 import com.husky.events.commands.command_manager.CommandExecutor;
 import com.husky.events.commands.shop.ShopCommand;
-import com.husky.events.commands.util.AvatarCommand;
-import com.husky.events.commands.util.HelloCommand;
-import com.husky.events.commands.util.PingCommand;
-import com.husky.events.commands.util.ProgressCommand;
+import com.husky.events.commands.util.*;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
@@ -25,6 +22,7 @@ public class Bot extends ListenerAdapter {
         commands.add(new ShopCommand());
         commands.add(new ProgressCommand());
         commands.add(new AvatarCommand());
+        commands.add(new ServerProfileCommand());
         Bot.jda = JDABuilder.createDefault(System.getenv("TOKEN"))
                 .setActivity(Activity.listening("?hello"))
                 .addEventListeners(new CommandExecutor(commands)).build();
