2cbab2f5a30407015a9b472cc7d097d2db55c839
OmiSingh2004 <omisinghdbz@gmail.com>
2021-10-08 15:46:19 +0530

    another oneee

--- src/main/java/com/husky/events/commands/util/AvatarCommand.java
+++ src/main/java/com/husky/events/commands/util/AvatarCommand.java
@@ -2,7 +2,9 @@ package com.husky.events.commands.util;
 
 import com.husky.events.commands.Command;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
 
 public class AvatarCommand extends Command {
 
@@ -13,9 +15,12 @@ public class AvatarCommand extends Command {
 
     @Override
     public void executeCommand(Message message) {
+        Member member = message.getMember();
+        if(message.getMentionedMembers().size() > 0)
+            member = message.getMentionedMembers(message.getGuild()).get(0);
         EmbedBuilder embed = new EmbedBuilder();
-        embed.setTitle(message.getMember().getNickname() + "'s Avatar");
-        embed.setImage(message.getAuthor().getAvatarUrl() + "?size=1024");
+        embed.setTitle(member.getNickname() + "'s Avatar");
+        embed.setImage(member.getUser().getAvatarUrl() + "?size=1024");
         message.replyEmbeds(embed.build()).queue();
     }
 }
