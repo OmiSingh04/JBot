5c862350c604741720ed014f221708fb6b096c00
Edantuti <edantuti@gmail.com>
2022-06-17 17:03:21 +0530

    ERASED THE TOKEN ITSELF

--- src/main/java/com/github/husky/Constant.java
+++ src/main/java/com/github/husky/Constant.java
@@ -2,9 +2,9 @@ package com.github.husky;
 
 public class Constant {
     public static final String PREFIX="!";
-    public static final String TOKEN = "OTYxNTc2ODQzNTgzMDQxNTk2.Yk7AOA.JuMDcojcrD4xTS-HCE3yKj7KT1M";
-    public static final String DB_USERNAME="vvvsgrvayrkfog";
-    public static final String DB_PASSWORD="44cc1400888dae689fae2a769d54e0b8c7d10af12afb30c23b552c47d9166318";
-    public static final String DB_URL="jdbc:postgresql://ec2-54-86-224-85.compute-1.amazonaws.com:5432/dag35jil1ng6uf"; 
+    public static final String TOKEN = "-";
+    public static final String DB_USERNAME="-";
+    public static final String DB_PASSWORD="-";
+    public static final String DB_URL="-"; 
     public static final String DB_PORT="5432";
 }
